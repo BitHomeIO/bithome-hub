6c4578d021891a1ad6540cce2e6b030dc7bb5b0b
Mike Kurdziel <michael.kurdziel@gmail.com>
2016-03-02 23:30:30 -0800

    Changed requires to not throw an error

--- app/services/MessageService.ts
+++ app/services/MessageService.ts
@@ -3,7 +3,7 @@ import {Message} from '../models/message';
 import {Subject} from 'rxjs/Subject';
 import {Observable} from 'rxjs/Observable';
 import {Operator} from 'rxjs/Operator';
-import {io} from '../typings/sails.io';
+import io = require('sails.io');
 import 'rxjs/rx';
 
 interface IMessagesOperation extends Function {
